42080f922079b833e426b212548e1a210b244ad7
Heath Winning (Sunshot) <heath.winning@sunshotenergy.com.au>
2021-02-26 14:42:07 +1100

    package it

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="simplesqlalchemy",
+    version="0.0.1",
+    author="Heath Winning",
+    description="A small wrapper around sqlalchemy for to reduce boilerplate with pandas",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+)
