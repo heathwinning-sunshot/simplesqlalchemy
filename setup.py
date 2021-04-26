ae798de264c697c9212563b57917af3583841a09
Heath Winning (Sunshot) <heath.winning@sunshotenergy.com.au>
2021-04-26 13:02:31 +1000

    python 3.7

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setuptools.setup(
     author="Heath Winning",
     description="A small wrapper around sqlalchemy to reduce boilerplate when using sqlalchemy and pandas.",
     packages=setuptools.find_packages(),
-    python_requires=">=3.8",
+    python_requires=">=3.7",
 )
