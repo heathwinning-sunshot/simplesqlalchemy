3b2a9f35411d749c9e7c691afee0967b4b44ffe6
Heath Winning <heath.winning@zenenergy.com.au>
2021-06-22 11:46:59 +1000

    minor refactor

--- simplesqlalchemy/simpledatabase.py
+++ simplesqlalchemy/simpledatabase.py
@@ -8,13 +8,15 @@ from sqlalchemy.orm import sessionmaker, Session
 from sqlalchemy.ext.automap import AutomapBase, automap_base, classname_for_table
 from sqlalchemy.orm.query import Query
 
+
 class Credentials:
     def __init__(self, uid: str, pwd: str) -> None:
         self.uid = uid
         self.pwd = pwd
 
+
 class Database:
-    def __init__(self, server: str, database: str, credentials: Credentials, fast_executemany: bool=True) -> None:
+    def __init__(self, server: str, database: str, credentials: Credentials, fast_executemany: bool = True) -> None:
         self.server: str = server
         self.database: str = database
         self.reset_metadata()
@@ -26,18 +28,26 @@ class Database:
     def t(self) -> SchemaCollection:
         return self.tables
 
-    def get_engine(self, credentials: Credentials, fast_executemany: bool=True) -> Engine:
-        driver = "{ODBC Driver 17 for SQL Server}"
-        connection_params = urllib.parse.quote_plus(f"DRIVER={driver};SERVER=tcp:{self.server},1433;DATABASE={self.database};UID={credentials.uid};PWD={credentials.pwd}")
-        connection_string = f"mssql+pyodbc:///?odbc_connect={connection_params}"
-        return create_engine(connection_string, fast_executemany=fast_executemany)
+    def get_engine(self, credentials: Credentials, fast_executemany: bool = True) -> Engine:
+        return create_engine(
+            connection_string=self.connection_string(
+                driver="{ODBC Driver 17 for SQL Server}",
+                credentials=credentials
+            ),
+            fast_executemany=fast_executemany)
 
-    def table(self, table_name: str, schema: str, columns: List[Column]=[], primary_key_columns: List[str]=[]):
+    def connection_string(self, driver: str, credentials: Credentials) -> str:
+        connection_params = urllib.parse.quote_plus(
+            f"DRIVER={driver};SERVER=tcp:{self.server},1433;DATABASE={self.database};UID={credentials.uid};PWD={credentials.pwd}")
+        return f"mssql+pyodbc:///?odbc_connect={connection_params}"
+
+    def table(self, table_name: str, schema: str, columns: List[Column] = [], primary_key_columns: List[str] = []):
         return Table(
             table_name,
             self.metadata,
             *columns,
-            *[Column(column, primary_key=True) for column in primary_key_columns],
+            *[Column(column, primary_key=True)
+              for column in primary_key_columns],
             autoload=True,
             autoload_with=self.engine,
             schema=schema
@@ -61,7 +71,7 @@ class Database:
     def to_df(self, query: Query) -> pd.DataFrame:
         return pd.read_sql(query.statement, self.engine)
 
-    def write_df(self, df: pd.DataFrame, table: Union[Table,str], **kwargs):
+    def write_df(self, df: pd.DataFrame, table: Union[Table, str], **kwargs):
         if isinstance(table, str):
             df.to_sql(
                 con=self.engine,
@@ -73,7 +83,8 @@ class Database:
                 try:
                     table = table.__table__
                 except AttributeError:
-                    raise ValueError(f"table of type {table.__class__} is not supported")
+                    raise ValueError(
+                        f"table of type {table.__class__} is not supported")
             df.to_sql(
                 con=self.engine,
                 name=table.name,
@@ -86,9 +97,10 @@ class Database:
             self.session.add_all(rows)
         else:
             self.session.add(rows)
-        
+
     def commit(self):
         self.session.commit()
+
 
 def schema_qualified_classname_for_table(base: AutomapBase, tablename: str, table: Table) -> str:
     return f"__{table.schema}__{tablename}"
